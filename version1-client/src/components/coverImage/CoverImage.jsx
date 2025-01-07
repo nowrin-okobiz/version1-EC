4fdb8a0cb06e8ab974c235277f4d596d6e094184
nowrin-okobiz <nowrin.okobiz@gmail.com>
2025-01-07 16:12:26 +0600

    make cover Image component
    make a component which take image url or path ,deliver the image

---
+++ version1-client/src/components/coverImage/CoverImage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function CoverImage({ image }) {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="" />
+      </div>
+    </div>
+  );
+}
